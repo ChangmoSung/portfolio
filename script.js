6112e9787027c35aac25f4b2662d2dbeb6d89881
ChangmoSung <changmosung0914@gmail.com>
2020-04-07 15:19:58 -0400

    added spinning flower to highlight only the project that people are looking at

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 $(function() {
     const $projectDescriptions = $('.description').toArray()
     const $projectImages = $('.projectImage').toArray();
-    const $nav = $('nav');
-
-    function changeNav() {
-        const scrolledPastHeader = window.innerHeight;
-        const pixelScrolled = window.scrollY;
-        
-        if(pixelScrolled > scrolledPastHeader) {
-            $nav.addClass('changeNav');
-        } else {
-            $nav.removeClass('changeNav');
-        }
-    }
+    const $overlayImages = $('.overlayImage').toArray();
+    let previousScroll;
 
     function slideProject() {
+        const scrollTop = $(this).scrollTop();
+
+        // $overlayImages.forEach(image => {
+        //     if (scrollTop > previousScroll) {
+        //         image.classList.add('active');
+        //     } else {
+        //         image.classList.remove('active');
+        //     }
+        // })
+
+        // previousScroll = scrollTop;
+
         $projectDescriptions.forEach(description => {
-            const slideAt = (window.scrollY + window.innerHeight) - description.offsetHeight / 2;
+            const slideAt = (scrollTop + window.innerHeight) - description.offsetHeight / 2;
             const elementBottom = description.offsetTop + description.offsetHeight;
             const halfShown = slideAt > description.offsetTop;
-            const notScrolledPast = window.scrollY < elementBottom;
+            const notScrolledPast = scrollTop < elementBottom;
 
             if (halfShown && notScrolledPast) {
                 description.classList.add('active');
@@ -29,19 +31,22 @@ $(function() {
         })
 
         $projectImages.forEach(image => {
-            const slideAt = (window.scrollY + window.innerHeight) - image.offsetHeight / 2;
+            const slideAt = (scrollTop + window.innerHeight) - image.offsetHeight / 2;
             const elementBottom = image.offsetTop + image.offsetHeight;
             const halfShown = slideAt > image.offsetTop;
-            const notScrolledPast = window.scrollY < elementBottom;
+            const notScrolledPast = scrollTop < elementBottom;
 
             if (halfShown && notScrolledPast) {
+                image.children[0].classList.add('active');
+                image.children[1].classList.add('active');
                 image.classList.add('active');
             } else {
                 image.classList.remove('active');
+                image.children[0].classList.remove('active');
+                image.children[1].classList.remove('active');
             }
         })
     }
 
-    $(window).on('scroll', changeNav);
     $(window).on('scroll', slideProject);
 });
