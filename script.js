c48b3312f37e8497c28c8467169a701676ec721b
ChangmoSung <changmosung0914@gmail.com>
2020-04-08 18:16:54 -0400

    header and nav responsiveness done

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ $(function() {
     const $projectDescriptions = $('.description').toArray()
     const $projectDesktopImages = $('.projectDesktopImage').toArray();
     const $projectMobileImages = $('.projectMobileImage').toArray();
+    const $nav = $('nav');
+    const $navLinks = $('nav a').toArray();
+
     function slideProject() {
         const scrollTop = $(this).scrollTop();
+        if(scrollTop > window.innerHeight - 60) {
+            $nav.addClass('changeColour');
+
+            $navLinks.forEach(link => {
+                link.classList.add('changeColour');
+            })
+        } else {
+            $nav.removeClass('changeColour');
+
+            $navLinks.forEach(link => {
+                link.classList.remove('changeColour');
+            })
+        }
 
         $projectDescriptions.forEach(description => {
             const slideAt = (scrollTop + window.innerHeight) - description.offsetHeight / 2;
