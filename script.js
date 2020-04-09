a3ffbf7ff1d9b13252926703b454bb25a6adefac
ChangmoSung <changmosung0914@gmail.com>
2020-04-09 04:47:53 -0400

    scrolling animation for project pages and colour for highlighted words changed

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(function() {
-    const $projectDescriptions = $('.description').toArray()
+    const $projectDescriptions = $('.description').toArray();
     const $projectDesktopImages = $('.projectDesktopImage').toArray();
     const $projectMobileImages = $('.projectMobileImage').toArray();
     const $nav = $('nav');
     const $navLists = $('.navList');
     const $navLinks = $('.navLink');
     const $hoverEffectSpans = $('.hoverEffectSpan');
+    const $videos = $('video').toArray();
 
     $navLists.on('mouseenter', function() {
         $(this).children().last().toggleClass('showHoverEffect');
@@ -14,6 +15,10 @@ $(function() {
     $navLists.on('mouseleave', function () {
         $(this).children().last().toggleClass('showHoverEffect');
     })
+
+    if(window.innerWidth <= 430) {
+        $videos.forEach(video => video.pause());
+    }
 
     $(window).on('scroll', function() {
         const scrollTop = $(this).scrollTop();
@@ -30,41 +35,31 @@ $(function() {
         }
 
         $projectDescriptions.forEach(description => {
-            const slideAt = (scrollTop + screenHeight) - description.offsetHeight / 2;
-            const elementBottom = description.offsetTop + description.offsetHeight;
-            const halfShown = slideAt > description.offsetTop;
-            const notScrolledPast = scrollTop < elementBottom;
-
-            if (halfShown && notScrolledPast) {
-                description.classList.add('active');
-            } else {
-                description.classList.remove('active');
-            }
+            scrollEvent(description)
         })
 
-        // ----- to spin flowers at certain pixel point ----- //
         $projectDesktopImages.forEach(image => {
-            spinFlower(image);
+            scrollEvent(image);
         })
 
         $projectMobileImages.forEach(image => {
-            spinFlower(image);
+            scrollEvent(image);
         })
 
-        function spinFlower(image) {
-            const slideAt = (scrollTop + screenHeight) - image.offsetHeight / 2;
-            const elementBottom = image.offsetTop + image.offsetHeight;
-            const halfShown = slideAt > image.offsetTop;
+        function scrollEvent(item) {
+            const slideAt = (scrollTop + screenHeight) - item.offsetHeight / 2;
+            const elementBottom = item.offsetTop + item.offsetHeight;
+            const halfShown = slideAt > item.offsetTop;
             const notScrolledPast = scrollTop < elementBottom;
 
             if (halfShown && notScrolledPast) {
-                image.children[0].classList.add('active');
-                image.children[1].classList.add('active');
-                image.classList.add('active');
+                item.classList.add('active');
+                item.children[0].classList.add('active');
+                item.children[1].classList.add('active');
             } else {
-                image.classList.remove('active');
-                image.children[0].classList.remove('active');
-                image.children[1].classList.remove('active');
+                item.classList.remove('active');
+                item.children[0].classList.remove('active');
+                item.children[1].classList.remove('active');
             }
         }
     });
